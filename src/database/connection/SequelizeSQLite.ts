109b417f463b368ed4408b8d9e3c53f0742245c3
Minh Trung <nguyenminhtrungse@gmail.com>
2024-08-10 13:43:30 +0700

    Add amplify.yml

--- src/database/connection/SequelizeSQLite.ts
+++ src/database/connection/SequelizeSQLite.ts
@@ -4,7 +4,9 @@ import { Sequelize } from "sequelize"
 
 export const DATABASE_PATH = path.resolve(process.cwd(), ".next", 'database.sqlite')
 
+console.log(path.resolve(process.cwd(), ".next"))
 if (!fs.existsSync(DATABASE_PATH)) {
+    console.log("CRETE")
     fs.writeFileSync(DATABASE_PATH, '')
 }
 const sequelize = new Sequelize(
